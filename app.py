c62a9659d0c05fef19d3e069731e8574b307ef31
Cristiano Verderio <verderio.cristiano@iisgalvanimi.edu.it>
2025-01-30 08:43:24 +0000

    finito con playlist visualizzata

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from spotipy.oauth2 import SpotifyOAuth
 #le tue credenziali le trovi nella dashboard di prima
 SPOTIFY_CLIENT_ID = "79c5d462922a41dca468f6b71d00a504"
 SPOTIFY_CLIENT_SECRET = "438ce2ed191742d3a0891ca9134def1a"
-SPOTIFY_REDIRECT_URI = "http://127.0.0.1:5000/callback" #dopo il login andiamo qui e va modificato
+SPOTIFY_REDIRECT_URI = "https://5000-verderiocri-progettinos-eityo7ljrli.ws-eu117.gitpod.io/callback" #dopo il login andiamo qui e va modificato
 
 app = Flask(__name__)
 app.secret_key = 'chiave_per_session' #ci serve per identificare la sessione
@@ -20,14 +20,19 @@ sp_oauth = SpotifyOAuth(
 @app.route('/')
 def login():
     auth_url = sp_oauth.get_authorize_url() #login di spotify
-return redirect(auth_url)
+    return redirect(auth_url)
 
 @app.route('/callback')
 def callback():
     code = request.args.get('code') #recupero codice di autorizzazione
     token_info = sp_oauth.get_access_token(code) #uso il code per un codice di accesso
     session['token_info'] = token_info #salvo il token nella mia sessione x riutilizzarlo
-return redirect(url_for('home'))
+    return redirect(url_for('home'))
+
+@app.route('/logout')
+def logout():
+    session.clear() #cancelliamo l'access token salvato in session
+    return redirect(url_for('login'))
 
 
 @app.route('/home')
@@ -37,7 +42,8 @@ def home():
         return redirect(url_for('login'))
     sp = spotipy.Spotify(auth=token_info['access_token']) #usiamo il token per ottenere i dati del profilo
     user_info = sp.current_user()
-    print(user_info) #capiamo la struttura di user_info per usarle nel frontend
-return render_template('home.html', user_info=user_info) #passo le info utente all'home.html
+    playlists = sp.current_user_playlists() #sempre tramite il token sp preso prima
+    playlists_info = playlists['items']
+    return render_template('home.html', user_info=user_info,playlists=playlists_info) #passo le info utente all'home.html
 
 app.run(debug = True)
